4d27d79b297fc2e7b8eb0712f830afc5f9dd0d13
Robert Brennan <contact@rbren.io>
2024-03-16 13:11:26 -0400

    remove logspam

--- lib/llm.py
+++ lib/llm.py
@@ -6,18 +6,11 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_openai import OpenAI
 
 def sendJSONPrompt(template, input={}):
-    print("make llm")
     llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
-    print("make prompt")
     prompt = PromptTemplate.from_template(template)
-    print("make chain")
     llm_chain = LLMChain(prompt=prompt, llm=llm)
-    print("make parser")
     parser = JsonOutputParser()
-    print("get resp")
     resp = llm_chain.invoke(input)
-    print("resp", resp)
     parsed = parser.parse(resp['text'])
-    print("parsed", parsed)
     return parsed
 
