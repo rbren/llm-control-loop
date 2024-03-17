a4097a8de3d06a4b64af7f0cdc929254d7b20751
Robert Brennan <contact@rbren.io>
2024-03-17 16:50:48 -0400

    fix browse

--- lib/actions/browse.py
+++ lib/actions/browse.py
@@ -1,3 +1,5 @@
+import requests
+
 def browse(url):
     response = requests.get(url)
     return response.text
--- lib/event.py
+++ lib/event.py
@@ -1,6 +1,5 @@
 import os
 import json
-import requests
 import lib.actions as actions
 
 class Event:
